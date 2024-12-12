b32aba7455282fbee79ff92ea7fade0385542b37
Gordius-Tech <160427572+Gordius-Tech@users.noreply.github.com>
2024-12-12 10:50:29 +0100

    Add files via upload

---
+++ pysource/elag14.py
@@ -0,0 +1,17 @@
+szam=int(input('Kérek egy számot 1-7 között: '))
+if szam==1:
+    print('Hétfő')
+elif szam==2:
+    print('Kedd')
+elif szam==3:
+    print('Szerda')
+elif szam==4:
+    print('Csütörtök')
+elif szam==5:
+    print('Péntek')
+elif szam==6:
+    print('Szombat')
+elif szam==7:
+    print('Vasárnap')
+else:
+    print('Hibás számot adtál meg :(')
